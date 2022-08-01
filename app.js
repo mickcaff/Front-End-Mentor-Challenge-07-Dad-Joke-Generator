4f7402ca81dca84e47bed8722df2255c42aa0aa2
mickcaff <mcaffery@outlook.com>
2022-08-01 20:42:55 +1000

    Changed API to dad jokes. Finished styling

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const button = document.querySelector('.button')
-const mainAdvice = document.querySelector('.advice-main')
-const idAdvice = document.querySelector('.advice-id')
+const mainAdvice = document.querySelector('.joke-main')
+const idAdvice = document.querySelector('.joke-id')
 
 const getAdvice = async () => {
     try {
-        const res = await fetch('https://api.adviceslip.com/advice')
+        const res = await fetch('https://icanhazdadjoke.com/', {
+            headers: {
+            'Accept': 'application/json'
+            }
+        })
         const data = await res.json()
-        console.log(data.slip)
+        console.log(data)
         return data
     } catch (e) {
         console.log("ERROR:". e)
@@ -15,10 +19,9 @@ const getAdvice = async () => {
 
 const addAdvice = async () => {
     const data = await getAdvice()
-    advice = data.slip.advice
-    id = data.slip.id
-    mainAdvice.textContent = advice
-    idAdvice.textContent = id
+    joke = data.joke
+    mainAdvice.textContent = joke
+    idAdvice.textContent = Math.ceil(Math.random()*10000)
 }
 
 button.addEventListener('click', addAdvice)
